34914c82e43a81d1db8d1b9b56dda9fb2253b02d
lzhabo <lidia.zhabo99@gmail.com>
2023-04-24 18:34:22 +0300

    added route that gives latest trades for pair of tokens

--- src/controllers/tradeController.ts
+++ src/controllers/tradeController.ts
@@ -1,8 +1,33 @@
 import { RequestHandler } from "express";
 import { Trade } from "../models/Trade";
+import { TOKENS_BY_SYMBOL } from "../constants";
 
 export const getAllTrades: RequestHandler<null> = async (req, res, next) => {
   const trades = await Trade.find({}).exec();
+  res.send(trades);
+};
+export const getLatestTradesForPair: RequestHandler<null> = async (req, res, next) => {
+  const value = req.params as any;
+  const symbol0 = value.symbol0;
+  const symbol1 = value.symbol1;
+  if (symbol0 == null || symbol1 == null) {
+    res.send([]);
+    return;
+  }
+  const trades = await Trade.find({
+    $or: [
+      {
+        asset0: TOKENS_BY_SYMBOL[symbol0].assetId,
+        asset1: TOKENS_BY_SYMBOL[symbol1].assetId,
+      },
+      {
+        asset0: TOKENS_BY_SYMBOL[symbol1].assetId,
+        asset1: TOKENS_BY_SYMBOL[symbol0].assetId,
+      },
+    ],
+  })
+    .limit(40)
+    .exec();
   res.send(trades);
 };
 export const createTrade: RequestHandler = async (req, res, next) => {
